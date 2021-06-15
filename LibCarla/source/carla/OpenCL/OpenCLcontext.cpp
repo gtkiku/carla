fcf14585b0800341ab8d1d05bf0f77415f45a2e5
Michal Babej <michal.babej@tuni.fi>
2021-06-15 04:42:42 +0200

    Separate OpenCL work into two kernels on two devices

--- LibCarla/source/carla/OpenCL/OpenCLcontext.cpp
+++ LibCarla/source/carla/OpenCL/OpenCLcontext.cpp
@@ -20,12 +20,12 @@
 
 #include "OpenCLcontext.h"
 
-static const char *SOURCE = R"(
+static const char *PX_COUNT_SOURCE = R"(
 
 #pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
 #pragma OPENCL EXTENSION cl_khr_int64_extended_atomics : enable
 
-__kernel void process_frame(global uchar4 *input, global ulong* output) {
+__kernel void process_frame(global const uchar4 *input, global ulong* output) {
     size_t x = get_global_id(0);
     size_t y = get_global_id(1);
     size_t width = get_global_size(0);
@@ -36,6 +36,46 @@ __kernel void process_frame(global uchar4 *input, global ulong* output) {
         atom_add(output, 1);
 }
 )";
+
+static const char *DOWNSAMPLE_SOURCE = R"(
+
+#pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
+#pragma OPENCL EXTENSION cl_khr_int64_extended_atomics : enable
+
+__kernel void downsample_image(global const uchar4 *input, global uchar4* output) {
+    size_t x = get_global_id(0);
+    size_t y = get_global_id(1);
+    size_t width = get_global_size(0);
+    size_t height = get_global_size(1);
+
+/*
+    uint4 sum = input[y * width + x]
+              + input[y * width + x + 1]
+              + input[(y+1) * width + x]
+              + input[(y+1) * width + x + 1];
+    output[y * (width/2) + x] = convert_uchar4(sum / 4);
+*/
+   unsigned out_idx = y + x*width;
+
+   const unsigned factor = 2;
+   width *= factor;
+   height *= factor;
+   unsigned y1, y2, x1, x2;
+   y1=y*factor;
+   y2=y1+1;
+   x1=x*factor;
+   x2=x1+1;
+
+   uchar4 q11, q12, q21, q22;
+   q11 = input[y1 * width + x1];
+   q12 = input[y1 * width + x2];
+   q21 = input[y2 * width + x1];
+   q22 = input[y2 * width + x2];
+   uint4 sum = convert_uint4(q11) + convert_uint4(q12) + convert_uint4(q21) + convert_uint4(q22);
+   output[out_idx] = convert_uchar4(sum/4);
+}
+)";
+
 
 #define OUTPUT_SIZE (cl::size_type)(sizeof(unsigned long)*64)
 
@@ -49,14 +89,19 @@ __kernel void process_frame(global uchar4 *input, global ulong* output) {
 class OpenCL_Context {
     cl::Platform platform;
     std::vector<cl::Device> devices;
-    cl::Device GpuDev;
-    cl::Context GpuContext;
-    cl::CommandQueue GpuQueue;
+    cl::Context ClContext;
 
+    cl::Device GpuDev;
+    cl::CommandQueue GpuQueue;
     cl::Program GpuProgram;
     cl::Kernel GpuKernel;
 
-    cl::Buffer InputBuffer, OutputBuffer;
+    cl::Device FpgaDev;
+    cl::CommandQueue FpgaQueue;
+    cl::Program FpgaProgram;
+    cl::Kernel FpgaKernel;
+
+    cl::Buffer GpuInputBuffer, InterBuffer, FpgaOutputBuffer;
     unsigned long InputBufferSize;
 
     std::mutex OclMutex;
@@ -68,12 +113,14 @@ public:
     OpenCL_Context() {};
 
     bool isAvailable() {
+      std::unique_lock<std::mutex> lock(OclMutex);
       if (!initialized)
           return false;
-      if (GpuDev() == nullptr)
+      if (GpuDev() == nullptr || FpgaDev() == nullptr)
           return false;
-      bool avail = GpuDev.getInfo<CL_DEVICE_AVAILABLE>() != CL_FALSE;
-      return avail;
+//      bool avail = GpuDev.getInfo<CL_DEVICE_AVAILABLE>() != CL_FALSE;
+//      return avail;
+      return true;
     }
 
     ~OpenCL_Context() {
@@ -81,15 +128,15 @@ public:
             shutdown();
     }
 
-    bool initialize(unsigned platID, unsigned devID, unsigned width, unsigned height, unsigned bpp);
+    bool initialize(unsigned width, unsigned height, unsigned bpp);
     bool processCameraFrame(unsigned char* input, unsigned long *output);
 
 private:
     void shutdown();
 };
 
-bool OpenCL_Manager::initialize(unsigned width, unsigned height, unsigned bpp, unsigned platID, unsigned devID) {
-    if (Context->initialize(platID, devID, width, height, bpp))
+bool OpenCL_Manager::initialize(unsigned width, unsigned height, unsigned bpp) {
+    if (Context->initialize(width, height, bpp))
         isValid = true;
     else {
         LOG_E("Error in OpenCL->initialize\n");
@@ -100,13 +147,14 @@ bool OpenCL_Manager::initialize(unsigned width, unsigned height, unsigned bpp, u
 
 bool OpenCL_Manager::processCameraFrame(unsigned char* input, unsigned long *output) {
 
-    std::unique_lock<std::mutex> lock(OclMutex);
     if (isValid) {
         if (!Context->processCameraFrame(input, output)) {
             LOG_E("Error in OpenCL->processCameraFrame\n");
             isValid = false;
         }
     }
+    if (!isValid)
+        *output = 11223344;
     return isValid;
 }
 
@@ -115,7 +163,7 @@ OpenCL_Manager::~OpenCL_Manager() {}
 
 
 
-bool OpenCL_Context::initialize(unsigned platID, unsigned devID, unsigned width, unsigned height, unsigned bpp)
+bool OpenCL_Context::initialize(unsigned width, unsigned height, unsigned bpp)
 {
     cl_int err;
     InputBufferSize = width * height * bpp / 8;
@@ -127,7 +175,7 @@ bool OpenCL_Context::initialize(unsigned platID, unsigned devID, unsigned width,
         return false;
     }
 
-    platform = all_platforms[platID];
+    platform = all_platforms[0];
     Offset = cl::NullRange;
 
     unsigned local_h = (height % 2) ? 1 : 2;
@@ -139,11 +187,11 @@ bool OpenCL_Context::initialize(unsigned platID, unsigned devID, unsigned width,
     local_w = (width % 8) ? local_w : 8;
     local_w = (width % 16) ? local_w : 16;
 
-    Local = cl::NDRange(local_w, local_h);
-    Global = cl::NDRange(width, height);
+    Local = cl::NDRange(local_w/2, local_h/2);
+    Global = cl::NDRange(width/2, height/2);
 
     // Find all devices.
-    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
     if(devices.size() == 0) {
         LOG_E("No OpenCL devices available!\n");
         return false;
@@ -165,25 +213,49 @@ bool OpenCL_Context::initialize(unsigned platID, unsigned devID, unsigned width,
     if (ptr == nullptr)
       LOG_I("platform doesn't support clEnqueueReadBufferContentPOCL\n");
     else
-//    enqueueReadBufferPOCL = (clEnqueueReadBufferContentPOCL_fn)ptr;
 
-    // Open a context for them.
-    GpuDev = devices[devID];
-    GpuContext = cl::Context(GpuDev, nullptr, nullptr, nullptr, &err);
+    if (devices.size() == 1) {
+        GpuDev = devices[0];
+        FpgaDev = devices[0];
+    } else if (devices[0].getInfo<CL_DEVICE_TYPE>() & CL_DEVICE_TYPE_GPU) {
+        GpuDev = devices[0];
+        FpgaDev = devices[1];
+    } else {
+        GpuDev = devices[1];
+        FpgaDev = devices[0];
+    }
+
+    ClContext = cl::Context(devices, nullptr, nullptr, nullptr, &err);
     CHECK_CL_ERROR(err, "Context creation failed\n");
-    GpuQueue = cl::CommandQueue(GpuContext, GpuDev, 0, &err); // , CL_QUEUE_PROFILING_ENABLE
-    CHECK_CL_ERROR(err, "CmdQueue creation failed\n");
 
-    GpuProgram = cl::Program{GpuContext, SOURCE, false, &err};
+    GpuQueue = cl::CommandQueue(ClContext, GpuDev, 0, &err); // , CL_QUEUE_PROFILING_ENABLE
+    CHECK_CL_ERROR(err, "CmdQueue creation failed\n");
+    if (devices.size() == 1)
+        FpgaQueue = GpuQueue;
+    else {
+        FpgaQueue = cl::CommandQueue(ClContext, FpgaDev, 0, &err); // , CL_QUEUE_PROFILING_ENABLE
+        CHECK_CL_ERROR(err, "CmdQueue creation failed\n");
+    }
+
+    GpuProgram = cl::Program{ClContext, DOWNSAMPLE_SOURCE, false, &err};
     CHECK_CL_ERROR(err, "Program creation failed\n");
     err = GpuProgram.build();
     CHECK_CL_ERROR(err, "Program build failed\n");
-    GpuKernel = cl::Kernel(GpuProgram, "process_frame", &err);
+    GpuKernel = cl::Kernel(GpuProgram, "downsample_image", &err);
     CHECK_CL_ERROR(err, "Kernel creation failed\n");
 
-    InputBuffer = cl::Buffer(GpuContext, CL_MEM_READ_WRITE, (cl::size_type)(InputBufferSize), nullptr, &err);
+    FpgaProgram = cl::Program{ClContext, PX_COUNT_SOURCE, false, &err};
+    CHECK_CL_ERROR(err, "Program creation failed\n");
+    err = FpgaProgram.build();
+    CHECK_CL_ERROR(err, "Program build failed\n");
+    FpgaKernel = cl::Kernel(FpgaProgram, "process_frame", &err);
+    CHECK_CL_ERROR(err, "Kernel creation failed\n");
+
+    GpuInputBuffer = cl::Buffer(ClContext, CL_MEM_READ_WRITE, (cl::size_type)(InputBufferSize), nullptr, &err);
     CHECK_CL_ERROR(err, "Input buffer creation failed\n");
-    OutputBuffer = cl::Buffer(GpuContext, CL_MEM_READ_WRITE, OUTPUT_SIZE, nullptr, &err);
+    InterBuffer = cl::Buffer(ClContext, CL_MEM_READ_WRITE, (cl::size_type)(InputBufferSize/4), nullptr, &err);
+    CHECK_CL_ERROR(err, "Inter buffer creation failed\n");
+    FpgaOutputBuffer = cl::Buffer(ClContext, CL_MEM_READ_WRITE, OUTPUT_SIZE, nullptr, &err);
     CHECK_CL_ERROR(err, "Output buffer creation failed\n");
 
 #ifdef ENABLE_COMPRESSION
@@ -194,8 +266,11 @@ bool OpenCL_Context::initialize(unsigned platID, unsigned devID, unsigned width,
     }
 #endif
 
-    GpuKernel.setArg(0, InputBuffer);
-    GpuKernel.setArg(1, OutputBuffer);
+    GpuKernel.setArg(0, GpuInputBuffer);
+    GpuKernel.setArg(1, InterBuffer);
+
+    FpgaKernel.setArg(0, InterBuffer);
+    FpgaKernel.setArg(1, FpgaOutputBuffer);
 
     initialized = true;
     return true;
@@ -207,26 +282,42 @@ bool OpenCL_Context::processCameraFrame(unsigned char* input, unsigned long *out
         return false;
     }
 
+    std::unique_lock<std::mutex> lock(OclMutex);
+
 #ifdef TIMING
     LOG_D("OpenCL: start processCameraFrame\n");
-    std::unique_lock<std::mutex> lock(OclMutex);
     auto start_time = std::chrono::steady_clock::now();
 #endif
 
     cl_int err;
-    err = GpuQueue.enqueueWriteBuffer(InputBuffer, CL_FALSE, 0, InputBufferSize, input);
-    if (err != CL_SUCCESS)
-        return false;
     *output = 0;
-    err = GpuQueue.enqueueWriteBuffer(OutputBuffer, CL_FALSE, 0, sizeof(cl_ulong), output);
+
+    std::vector<cl::Event> evts;
+    cl::Event ev1, ev2, ev3, ev4;
+
+    err = GpuQueue.enqueueWriteBuffer(GpuInputBuffer, CL_FALSE, 0, InputBufferSize, input, nullptr, &ev1);
     if (err != CL_SUCCESS)
         return false;
 
-    err = GpuQueue.enqueueNDRangeKernel(GpuKernel, Offset, Global, Local);
+    evts.push_back(ev1);
+    err = GpuQueue.enqueueNDRangeKernel(GpuKernel, Offset, Global, Local, &evts, &ev2);
     if (err != CL_SUCCESS)
         return false;
 
-    err = GpuQueue.enqueueReadBuffer(OutputBuffer, CL_TRUE, 0, sizeof(unsigned long), output);
+    err = FpgaQueue.enqueueWriteBuffer(FpgaOutputBuffer, CL_FALSE, 0, sizeof(cl_ulong), output, nullptr, &ev3);
+    if (err != CL_SUCCESS)
+        return false;
+
+    evts.clear();
+    evts.push_back(ev2);
+    evts.push_back(ev3);
+    err = FpgaQueue.enqueueNDRangeKernel(FpgaKernel, Offset, Global, Local, &evts, &ev4);
+    if (err != CL_SUCCESS)
+        return false;
+
+    evts.clear();
+    evts.push_back(ev4);
+    err = GpuQueue.enqueueReadBuffer(FpgaOutputBuffer, CL_TRUE, 0, sizeof(unsigned long), output, &evts);
     if (err != CL_SUCCESS)
         return false;
 
